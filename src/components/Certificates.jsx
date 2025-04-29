9afac3aad5c2ac4392a9f9d366c13306b2c38938
Marcos <turcismarcos@gmail.com>
2025-04-29 11:39:02 -0300

    estilo final nombre e info

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -34,7 +34,7 @@ export default function Certificates() {
       <div className="row">
         {certificates.map((cert, index) => (
           <div key={index} className="col-md-6 mb-4 d-flex">
-            <div className="bg-dark p-3 rounded certificado d-flex flex-column justify-content-between w-100">
+            <div className="bg-dark p-4 rounded certificado d-flex flex-column justify-content-between w-100">
               <div>
                 <img src={cert.image} alt={cert.title} className="img-fluid rounded mb-3" />
                 <h5>{cert.title}</h5>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 function Hero() {
   return (
     <section className="hero">
-      <div className="hero-content text-center">
-        <h1>
+      <div className="hero-content text-center d-flex flex-column justify-content-center align-items-center vh-100">
+        <h1 className="text-white fs-1 fs-md-display-4 fs-lg-display-1">
           Marcos Turcis<span className="cursor"></span>
         </h1>
-        <h2 className="subtitle">Estudiante de Ingenieria Informatica</h2>
-        <h2 className="subtitle">Programador - Diseñador</h2>
+        <h2 className="subtitle text-secondary fs-4 fs-md-4 mb-2">Estudiante de Ingenieria Informatica</h2>
+        <h2 className="subtitle text-secondary fs-5 fs-md-4">Programador - Diseñador</h2>
       </div>
     </section>
   )
